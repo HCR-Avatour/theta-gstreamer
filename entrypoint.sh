fab2b7508f4aad20d0f3945c60982241b641a8d3
Harry Phillips <harryjamesphillips@gmail.com>
2024-02-08 12:07:48 +0000

    Go to 2K streaming for now to save bandwidth

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gst-launch-1.0 thetauvcsrc mode=4K ! h264parse ! rtspclientsink location=rtsp://mediamtx:8554/theta
+gst-launch-1.0 thetauvcsrc mode=2K ! h264parse ! rtspclientsink location=rtsp://mediamtx:8554/theta
