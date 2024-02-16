21b430e03f9d23f9d0979aab77791f8d45e80bf7
Harry Phillips <harryjamesphillips@gmail.com>
2024-02-16 16:28:18 +0000

    Re-encode stream using hardware acceleration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gst-launch-1.0 thetauvcsrc mode=4K ! h264parse ! rtspclientsink location=rtsp://mediamtx:8554/theta
+gst-launch-1.0 thetauvcsrc mode=4K ! h264parse ! nvv4l2decoder ! nvv4l2h264enc bitrate=1000000 ! h264parse ! rtspclientsink location=rtsp://mediamtx:8554/theta
