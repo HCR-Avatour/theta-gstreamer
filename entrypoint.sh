448b3343f52405b15b33d56f1e7ed97f90b533b5
Harry Phillips <harryjamesphillips@gmail.com>
2024-03-12 18:04:28 +0000

    Mulitplexed pipeline for audio and video

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-gst-launch-1.0 thetauvcsrc mode=4K ! h264parse ! nvv4l2decoder ! nvv4l2h264enc bitrate=10000000 iframeinterval=15 ! h264parse ! rtspclientsink location=rtsp://mediamtx:8554/theta
+MIC_CARD_NUMBER=$(aplay -l | grep ReSpeaker | sed -nE 's/card ([0-9]+):.+/\1/p')
+
+VIDEO_SOURCE="thetauvcsrc mode=4K ! h264parse ! nvv4l2decoder ! nvv4l2h264enc bitrate=10000000 iframeinterval=15 ! h264parse"
+AUDIO_SOURCE="alsasrc device=hw:$MIC_CARD_NUMBER"
+STREAM_SINK="rtspclientsink location=rtsp://mediamtx:8554/theta"
+MUX_PIPELINE="$VIDEO_SOURCE ! mux. $AUDIO_SOURCE ! mux. matroskamux name=mux ! $STREAM_SINK"
+
+gst-launch-1.0 $MUX_PIPELINE
